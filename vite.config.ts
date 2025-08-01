25392514f7c3b865bda3f329672e304a2ffd667f
LimKemHout8989 <114142197+LimKemHout8989@users.noreply.github.com>
2025-08-01 07:55:24 +0000

    Update

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    allowedHosts: [
+      '83c1abcf-9358-42ab-ac72-7de07daeb08d-00-1ww7zrmucw514.picard.replit.dev'
+    ]
+  },
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        // This will transform your SVG to a React component
+        exportType: "named",
+        namedExport: "ReactComponent",
+      },
+    }),
+  ],
+});
